dd309d2f01f03a68c32e6580bb105d942dca66a5
hongxiang <hongxhu@umich.edu>
2024-07-05 18:22:24 -0400

    update

--- src/description2embedding.py
+++ src/description2embedding.py
@@ -9,65 +9,85 @@ from transformers import AutoTokenizer, AutoModel
 import torch
 
 
-# collect description info from study file
-def collect_description(file_data):
-    collected_data = {}
+# Extracts the study ID and description from the given file data.
+def extract_description(file_data):
+    description_extracted = {}
     
     try:
-        collected_data['nctId'] = file_data['protocolSection']['identificationModule']['nctId']
+        description_extracted['nctId'] = file_data['protocolSection']['identificationModule']['nctId']
     except:
-        collected_data['nctId'] = np.nan
+        description_extracted['nctId'] = np.nan
     
     try:
-        collected_data['description'] = file_data['protocolSection']['descriptionModule']['detailedDescription']
+        description_extracted['description'] = file_data['protocolSection']['descriptionModule']['detailedDescription']
     except:
         try:
-            collected_data['description'] = file_data['protocolSection']['descriptionModule']['briefSummary']
+            description_extracted['description'] = file_data['protocolSection']['descriptionModule']['briefSummary']
         except:
-            collected_data['description'] = np.nan
+            description_extracted['description'] = np.nan
     
-    return collected_data
+    return description_extracted
 
 
-# Load BioBERT tokenizer and model
-tokenizer = AutoTokenizer.from_pretrained("dmis-lab/biobert-base-cased-v1.1")
-model = AutoModel.from_pretrained("dmis-lab/biobert-base-cased-v1.1")
+# Generates embeddings for study descriptions using BioBERT.
+def description2embedding(input_path):
+    embedding_data = []
+
+    # Load BioBERT tokenizer and model
+    tokenizer = AutoTokenizer.from_pretrained("dmis-lab/biobert-base-cased-v1.1")
+    model = AutoModel.from_pretrained("dmis-lab/biobert-base-cased-v1.1")
+
+    # generat the train-test-incompleted ids
+    train_ids_path = os.path.join(input_path, 'train_ids.csv')
+    test_ids_path = os.path.join(input_path, 'test_ids.csv')
+    incompleted_ids_path = os.path.join(input_path, 'incompleted_ids.csv')
+
+    train_ids = pd.read_csv(train_ids_path).nctId.tolist()
+    test_ids = pd.read_csv(test_ids_path).nctId.tolist()
+    incompleted_ids = pd.read_csv(incompleted_ids_path).nctId.tolist()
+
+    all_ids = train_ids + test_ids + incompleted_ids
+
+    # generate embedding
+    for study_id in tqdm(all_ids):
+        filepath = os.path.join(input_path, 'ctg-studies.json', f'{study_id}.json')
+
+        try:
+            with open(filepath, 'r') as file:
+                file_data = json.load(file)
+                description_extracted = extract_description(file_data)
+                nctId = description_extracted['nctId']
+                description = description_extracted['description']
+
+                if pd.notna(description):
+                    # Tokenize and encode the description
+                    inputs = tokenizer(description, return_tensors='pt', padding=True,
+                                    truncation=True, max_length=512)
+                    outputs = model(**inputs)
+                    description_embedding = outputs.last_hidden_state[:, 0, :].detach().numpy()
+
+                    # append nctId and embedding data
+                    embedding_data.append({'nctId': nctId,
+                                           'description_embedding': description_embedding})
+                else:
+                    print(f'No valid description found for {study_id}')
+
+        except Exception as e:
+            print(f"An unexpected error occurred while processing {study_id}: {e}")
+    
+    return embedding_data
 
 
-# transfer description to embeddings by BioBERT
-input_path = './data_example/ctg-studies.json'
-output_path = './data_example/description2embedding.pkl'
+def main():
+    input_path = './data_example'
+    output_path = './data_example/description2embedding.pkl'
 
-json_files = [f for f in os.listdir(input_path) if f.endswith('.json')]
-final_data = []
-
-for json_file in tqdm(json_files[:10]):
-    filepath = os.path.join(input_path, json_file)
-
-    try:
-        with open(filepath, 'r') as file:
-            file_data = json.load(file)
-            collected_data = collect_description(file_data)
-            description = collected_data['description']
-
-            # Tokenize and encode the description
-            inputs = tokenizer(description, return_tensors='pt', padding=True,
-                               truncation=True, max_length=512)
-            outputs = model(**inputs)
-            description_embedding = outputs.last_hidden_state[:, 0, :].detach().numpy()
-
-            # update the description to embeddings
-            collected_data['description'] = description_embedding
-            final_data.append(collected_data)
-
-    except FileNotFoundError:
-        print(f"Error: The file {json_file} was not found.")
-    except Exception as e:
-        print(f"An unexpected error occurred while processing {json_file}: {e}")
+    embedding_data = description2embedding(input_path)
+    with open(output_path, 'wb') as file:
+        pickle.dump(embedding_data, file)
 
 
-# save data
-with open(output_path, 'wb') as file:
-    pickle.dump(final_data, file)
+if __name__ == '__main__':
+    main()
 
 

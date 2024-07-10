10f348e787e3a8d86643d839a95d13f7fb96541c
97dankos <dankos@umich.edu>
2024-07-09 23:27:59 -0400

    added enrollment # and exclusion criteria to description2embedding

--- src/description2embedding.py
+++ src/description2embedding.py
@@ -1,4 +1,4 @@
-# this script is to parse raw data to 
+# this script is to parse raw data to
 import numpy as np
 import pandas as pd
 import json
@@ -7,25 +7,88 @@ from tqdm import tqdm
 import pickle
 from transformers import AutoTokenizer, AutoModel
 import torch
+import re
 
 
 # Extracts the study ID and description from the given file data.
 def extract_description(file_data):
     description_extracted = {}
-    
+
     try:
-        description_extracted['nctId'] = file_data['protocolSection']['identificationModule']['nctId']
+        description_extracted["nctId"] = file_data["protocolSection"][
+            "identificationModule"
+        ]["nctId"]
     except:
-        description_extracted['nctId'] = np.nan
-    
+        description_extracted["nctId"] = np.nan
+
     try:
-        description_extracted['description'] = file_data['protocolSection']['descriptionModule']['detailedDescription']
+        description_extracted["description"] = file_data["protocolSection"][
+            "descriptionModule"
+        ]["detailedDescription"]
     except:
         try:
-            description_extracted['description'] = file_data['protocolSection']['descriptionModule']['briefSummary']
+            description_extracted["description"] = file_data["protocolSection"][
+                "descriptionModule"
+            ]["briefSummary"]
         except:
-            description_extracted['description'] = np.nan
-    
+            description_extracted["description"] = np.nan
+
+    # Enrollment
+    try:
+        description_extracted["enrollment"] = file_data["protocolSection"][
+            "designModule"
+        ]["enrollmentInfo"]["count"]
+    except:
+        description_extracted["enrollment"] = np.nan
+
+    # Eligibility Criteria, splitting inclusion and exclusion criteria
+    try:
+        eligibility = re.split(
+            (r"(?i)exclusion\b.\bcriteria\b"),
+            file_data["protocolSection"]["eligibilityModule"]["eligibilityCriteria"],
+        )
+    except:
+        description_extracted["inclusion_criteria"] = np.nan
+        description_extracted["exclusion_criteria"] = np.nan
+
+    try:
+        description_extracted["inclusion_criteria"] = eligibility[0]
+        description_extracted["exclusion_criteria"] = eligibility[1]
+    except:
+        try:
+            description_extracted["inclusion_criteria"] = eligibility[0]
+            description_extracted["exclusion_criteria"] = np.nan
+        except:
+            description_extracted["inclusion_criteria"] = np.nan
+            description_extracted["exclusion_criteria"] = np.nan
+
+    # # Treatments
+    # try:
+    #     description_extracted['treatment'] =
+    # except:
+    #     try:
+    #         description_extracted['treatment'] =
+    #     except:
+    #         description_extracted['treatment'] = np.nan
+
+    # # Disease
+    # try:
+    #     description_extracted['disease'] =
+    # except:
+    #     try:
+    #         description_extracted['disease'] =
+    #     except:
+    #         description_extracted['disease'] = np.nan
+
+    # # Outcome Measures
+    # try:
+    #     description_extracted['outcome_measures'] =
+    # except:
+    #     try:
+    #         description_extracted['outcome_measures'] =
+    #     except:
+    #         description_extracted['outcome_measures'] = np.nan
+
     return description_extracted
 
 
@@ -37,10 +100,10 @@ def description2embedding(input_path):
     tokenizer = AutoTokenizer.from_pretrained("dmis-lab/biobert-base-cased-v1.1")
     model = AutoModel.from_pretrained("dmis-lab/biobert-base-cased-v1.1")
 
-    # generat the train-test-incompleted ids
-    train_ids_path = os.path.join(input_path, 'train_ids.csv')
-    test_ids_path = os.path.join(input_path, 'test_ids.csv')
-    incompleted_ids_path = os.path.join(input_path, 'incompleted_ids.csv')
+    # generate the train-test-incompleted ids
+    train_ids_path = os.path.join(input_path, "train_ids.csv")
+    test_ids_path = os.path.join(input_path, "test_ids.csv")
+    incompleted_ids_path = os.path.join(input_path, "incompleted_ids.csv")
 
     train_ids = pd.read_csv(train_ids_path).nctId.tolist()
     test_ids = pd.read_csv(test_ids_path).nctId.tolist()
@@ -50,44 +113,85 @@ def description2embedding(input_path):
 
     # generate embedding
     for study_id in tqdm(all_ids):
-        filepath = os.path.join(input_path, 'ctg-studies.json', f'{study_id}.json')
+        filepath = os.path.join(input_path, "ctg-studies.json", f"{study_id}.json")
 
         try:
-            with open(filepath, 'r') as file:
+            with open(filepath, "r") as file:
                 file_data = json.load(file)
                 description_extracted = extract_description(file_data)
-                nctId = description_extracted['nctId']
-                description = description_extracted['description']
+                nctId = description_extracted["nctId"]
+                description = description_extracted["description"]
+
+                enrollment = description_extracted["enrollment"]
+                inclusion = description_extracted["inclusion_criteria"]
+                exclusion = description_extracted["exclusion_criteria"]
 
                 if pd.notna(description):
                     # Tokenize and encode the description
-                    inputs = tokenizer(description, return_tensors='pt', padding=True,
-                                    truncation=True, max_length=512)
+                    inputs = tokenizer(
+                        description,
+                        return_tensors="pt",
+                        padding=True,
+                        truncation=True,
+                        max_length=512,
+                    )
                     outputs = model(**inputs)
-                    description_embedding = outputs.last_hidden_state[:, 0, :].detach().numpy()
+                    description_embedding = (
+                        outputs.last_hidden_state[:, 0, :].detach().numpy()
+                    )
+
+                    # Tokenize and encode the eligibility criteria
+                    inputs = tokenizer(
+                        inclusion,
+                        return_tensors="pt",
+                        padding=True,
+                        truncation=True,
+                        max_length=512,
+                    )
+                    outputs = model(**inputs)
+                    inclusion_embedding = (
+                        outputs.last_hidden_state[:, 0, :].detach().numpy()
+                    )
+
+                    inputs = tokenizer(
+                        exclusion,
+                        return_tensors="pt",
+                        padding=True,
+                        truncation=True,
+                        max_length=512,
+                    )
+                    outputs = model(**inputs)
+                    exclusion_embedding = (
+                        outputs.last_hidden_state[:, 0, :].detach().numpy()
+                    )
 
                     # append nctId and embedding data
-                    embedding_data.append({'nctId': nctId,
-                                           'description_embedding': description_embedding})
+                    embedding_data.append(
+                        {
+                            "nctId": nctId,
+                            "enrollment": enrollment,
+                            "description_embedding": description_embedding,
+                            "inclusion_embedding": inclusion_embedding,
+                            "exclusion_embedding": exclusion_embedding,
+                        }
+                    )
                 else:
-                    print(f'No valid description found for {study_id}')
+                    print(f"No valid description found for {study_id}")
 
         except Exception as e:
             print(f"An unexpected error occurred while processing {study_id}: {e}")
-    
+
     return embedding_data
 
 
 def main():
-    input_path = './data_example'
-    output_path = './data_example/description2embedding.pkl'
+    input_path = "./data_example"
+    output_path = "./data_example/description2embedding.pkl"
 
     embedding_data = description2embedding(input_path)
-    with open(output_path, 'wb') as file:
+    with open(output_path, "wb") as file:
         pickle.dump(embedding_data, file)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
-

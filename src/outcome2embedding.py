9c29e146f3825b74aa5a3d20bce6a570c0e10e24
hongxiang <hongxhu@umich.edu>
2024-07-16 18:55:18 -0400

    update the script with embedding details

--- src/outcome2embedding.py
+++ src/outcome2embedding.py
@@ -1,6 +1,4 @@
-# HAVE NOT DONE EMBEDDING YET, SO ALL COMMENTED OUT
-
-# this script is to parse raw data to
+# this script is to embed primary outcomes
 import numpy as np
 import pandas as pd
 import json
@@ -9,7 +7,6 @@ from tqdm import tqdm
 import pickle
 from transformers import AutoTokenizer, AutoModel
 import torch
-import re
 
 
 # Extracts the study ID and primary outcome measures from the given file data.
@@ -23,97 +20,83 @@ def extract_outcome(file_data):
     except:
         outcome_extracted["nctId"] = np.nan
 
-    # Outcome Measures (measure & timeframe)
+    # Select primary outcome (measure & timeframe)
     outcomes = file_data["protocolSection"]["outcomesModule"]["primaryOutcomes"]
     try:
         for o in outcomes:
             try:
-                outcome_extracted["primary_outcome_measure"].append(o["measure"])
+                outcome_extracted["outcome_measures"].append(o["measure"])
+                outcome_extracted["outcome_timeframes"].append(o["timeFrame"])
             except:
-                outcome_extracted["primary_outcome_measure"] = [o["measure"]]
+                outcome_extracted["outcome_measures"] = [o["measure"]]
+                outcome_extracted["outcome_timeframes"] = [o["timeFrame"]]
     except:
-        outcome_extracted["primary_outcome_measure"] = np.nan
-
-    try:
-        outcome_extracted["primary_outcome_timeframe"] = outcomes["timeFrame"]
-    except:
-        outcome_extracted["primary_outcome_timeframe"] = np.nan
+        outcome_extracted["outcome_measures"] = np.nan
+        outcome_extracted["outcome_timeframes"] = np.nan
 
     return outcome_extracted
 
 
-# Generates embeddings for outcome measures using ***.
-# def outcome2embedding(input_path):
-#     embedding_data = []
+# generate the train-test-incompleted ids
+def get_all_ids(input_path):
+    train_ids_path = os.path.join(input_path, "train_ids.csv")
+    test_ids_path = os.path.join(input_path, "test_ids.csv")
+    incompleted_ids_path = os.path.join(input_path, "incompleted_ids.csv")
 
-#     # Load BioBERT tokenizer and model
-#     tokenizer = AutoTokenizer.from_pretrained("dmis-lab/biobert-base-cased-v1.1")
-#     model = AutoModel.from_pretrained("dmis-lab/biobert-base-cased-v1.1")
+    train_ids = pd.read_csv(train_ids_path).nctId.tolist()
+    test_ids = pd.read_csv(test_ids_path).nctId.tolist()
+    incompleted_ids = pd.read_csv(incompleted_ids_path).nctId.tolist()
 
-#     # generate the train-test-incompleted ids
-#     train_ids_path = os.path.join(input_path, "train_ids.csv")
-#     test_ids_path = os.path.join(input_path, "test_ids.csv")
-#     incompleted_ids_path = os.path.join(input_path, "incompleted_ids.csv")
+    all_ids = train_ids + test_ids + incompleted_ids
 
-#     train_ids = pd.read_csv(train_ids_path).nctId.tolist()
-#     test_ids = pd.read_csv(test_ids_path).nctId.tolist()
-#     incompleted_ids = pd.read_csv(incompleted_ids_path).nctId.tolist()
+    return all_ids
 
-#     all_ids = train_ids + test_ids + incompleted_ids
 
-#     # generate embedding
-#     for study_id in tqdm(all_ids):
-#         filepath = os.path.join(input_path, "ctg-studies.json", f"{study_id}.json")
+# Generate embeddings for a list of text entries.
+def generate_embeddings(tokenizer, model, texts):
+    embeddings = np.zeros((len(texts), 768))
+    for i, text in enumerate(texts):
+        inputs = tokenizer(text, return_tensors="pt", padding=True,
+                           truncation=True, max_length=512)
+        outputs = model(**inputs)
+        embeddings[i] = outputs.last_hidden_state[:, 0, :].detach().numpy()
+    return embeddings.mean(axis=0)
 
-#         try:
-#             with open(filepath, "r") as file:
-#                 file_data = json.load(file)
-#                 outcome_extracted = extract_outcome(file_data)
-#                 nctId = outcome_extracted["nctId"]
-#                 inclusion = outcome_extracted["inclusion_criteria"]
-#                 exclusion = outcome_extracted["exclusion_criteria"]
 
-#                 if pd.notna(inclusion):
-#                     # Tokenize and encode the eligibility criteria
-#                     inputs = tokenizer(
-#                         inclusion,
-#                         return_tensors="pt",
-#                         padding=True,
-#                         truncation=True,
-#                         max_length=512,
-#                     )
-#                     outputs = model(**inputs)
-#                     inclusion_embedding = (
-#                         outputs.last_hidden_state[:, 0, :].detach().numpy()
-#                     )
+# Generates embeddings for outcome measures and timeframes using BioBERT
+def outcome2embedding(input_path):
+    embedding_data = []
+    all_ids = get_all_ids(input_path)
 
-#                     inputs = tokenizer(
-#                         exclusion,
-#                         return_tensors="pt",
-#                         padding=True,
-#                         truncation=True,
-#                         max_length=512,
-#                     )
-#                     outputs = model(**inputs)
-#                     exclusion_embedding = (
-#                         outputs.last_hidden_state[:, 0, :].detach().numpy()
-#                     )
+    # Load BioBERT tokenizer and model
+    tokenizer = AutoTokenizer.from_pretrained("dmis-lab/biobert-base-cased-v1.1")
+    model = AutoModel.from_pretrained("dmis-lab/biobert-base-cased-v1.1")
 
-#                     # append nctId and embedding data
-#                     embedding_data.append(
-#                         {
-#                             "nctId": nctId,
-#                             "inclusion_embedding": inclusion_embedding,
-#                             "exclusion_embedding": exclusion_embedding,
-#                         }
-#                     )
-#                 else:
-#                     print(f"No valid description found for {study_id}")
+    # generate embedding
+    for study_id in tqdm(all_ids):
+        filepath = os.path.join(input_path, "ctg-studies.json", f"{study_id}.json")
 
-#         except Exception as e:
-#             print(f"An unexpected error occurred while processing {study_id}: {e}")
+        try:
+            with open(filepath, "r") as file:
+                file_data = json.load(file)
+                outcome_extracted = extract_outcome(file_data)
 
-#     return embedding_data
+                if isinstance(outcome_extracted["outcome_measures"], list):
+                    measures_embedding = generate_embeddings(tokenizer, model,
+                                                             outcome_extracted["outcome_measures"])
+                    timeframes_embedding = generate_embeddings(tokenizer, model,
+                                                               outcome_extracted["outcome_timeframes"])
+                    embedding_data.append({
+                        "nctId": outcome_extracted["nctId"],
+                        "measures_embedding": measures_embedding,
+                        "timeframes_embedding": timeframes_embedding,
+                    })
+                else:
+                    print(f"No valid disease found for {study_id}")
+        except Exception as e:
+            print(f"An unexpected error occurred while processing {study_id}: {e}")
+
+    return embedding_data
 
 
 def main():

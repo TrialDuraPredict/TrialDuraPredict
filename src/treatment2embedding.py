0a7eb167bff40bc5c8ad8ed0cf46259060f2aec5
97dankos <dankos@umich.edu>
2024-07-19 14:48:48 -0400

    combined all embedding files into one: embeddings_df, and embedded treatments using BioBERT as string separated by commas

--- src/treatment2embedding.py
+++ src/treatment2embedding.py
@@ -1,6 +1,3 @@
-# HAVE NOT DONE EMBEDDING YET, SO ALL COMMENTED OUT
-
-
 # this script is to parse raw data to
 import numpy as np
 import pandas as pd
@@ -34,6 +31,11 @@ def extract_treatment(file_data):
                 treatment_extracted["treatment_type"].append(t["type"])
             except:
                 treatment_extracted["treatment_type"] = [t["type"]]
+
+        if type(treatment_extracted["treatment_type"]) == list:
+            treatment_extracted["treatment_type"] = ", ".join(
+                treatment_extracted["treatment_type"]
+            )
     except:
         treatment_extracted["treatment_type"] = np.nan
 
@@ -41,77 +43,63 @@ def extract_treatment(file_data):
 
 
 # Generates embeddings for treatment type using ***.
-# def treatment2embedding(input_path):
-#     embedding_data = []
+def treatment2embedding(input_path):
+    embedding_data = []
 
-#     # Load BioBERT tokenizer and model
-#     tokenizer = AutoTokenizer.from_pretrained("dmis-lab/biobert-base-cased-v1.1")
-#     model = AutoModel.from_pretrained("dmis-lab/biobert-base-cased-v1.1")
+    # Load BioBERT tokenizer and model
+    tokenizer = AutoTokenizer.from_pretrained("dmis-lab/biobert-base-cased-v1.1")
+    model = AutoModel.from_pretrained("dmis-lab/biobert-base-cased-v1.1")
 
-#     # generate the train-test-incompleted ids
-#     train_ids_path = os.path.join(input_path, "train_ids.csv")
-#     test_ids_path = os.path.join(input_path, "test_ids.csv")
-#     incompleted_ids_path = os.path.join(input_path, "incompleted_ids.csv")
+    # generate the train-test-incompleted ids
+    train_ids_path = os.path.join(input_path, "train_ids.csv")
+    test_ids_path = os.path.join(input_path, "test_ids.csv")
+    incompleted_ids_path = os.path.join(input_path, "incompleted_ids.csv")
 
-#     train_ids = pd.read_csv(train_ids_path).nctId.tolist()
-#     test_ids = pd.read_csv(test_ids_path).nctId.tolist()
-#     incompleted_ids = pd.read_csv(incompleted_ids_path).nctId.tolist()
+    train_ids = pd.read_csv(train_ids_path).nctId.tolist()
+    test_ids = pd.read_csv(test_ids_path).nctId.tolist()
+    incompleted_ids = pd.read_csv(incompleted_ids_path).nctId.tolist()
 
-#     all_ids = train_ids + test_ids + incompleted_ids
+    all_ids = train_ids + test_ids + incompleted_ids
 
-#     # generate embedding
-#     for study_id in tqdm(all_ids):
-#         filepath = os.path.join(input_path, "ctg-studies.json", f"{study_id}.json")
+    # generate embedding
+    for study_id in tqdm(all_ids):
+        filepath = os.path.join(input_path, "ctg-studies.json", f"{study_id}.json")
 
-#         try:
-#             with open(filepath, "r") as file:
-#                 file_data = json.load(file)
-#                 treatment_extracted = extract_treatment(file_data)
-#                 nctId = treatment_extracted["nctId"]
-#                 inclusion = treatment_extracted["inclusion_criteria"]
-#                 exclusion = treatment_extracted["exclusion_criteria"]
+        try:
+            with open(filepath, "r") as file:
+                file_data = json.load(file)
+                treatment_extracted = extract_treatment(file_data)
+                nctId = treatment_extracted["nctId"]
+                treatment = treatment_extracted["treatment_type"]
 
-#                 if pd.notna(inclusion):
-#                     # Tokenize and encode the eligibility criteria
-#                     inputs = tokenizer(
-#                         inclusion,
-#                         return_tensors="pt",
-#                         padding=True,
-#                         truncation=True,
-#                         max_length=512,
-#                     )
-#                     outputs = model(**inputs)
-#                     inclusion_embedding = (
-#                         outputs.last_hidden_state[:, 0, :].detach().numpy()
-#                     )
+                if pd.notna(treatment):
+                    # Tokenize and encode the eligibility criteria
+                    inputs = tokenizer(
+                        treatment,
+                        return_tensors="pt",
+                        padding=True,
+                        truncation=True,
+                        max_length=512,
+                    )
+                    outputs = model(**inputs)
+                    treatment_embedding = (
+                        outputs.last_hidden_state[:, 0, :].detach().numpy()
+                    )
 
-#                     inputs = tokenizer(
-#                         exclusion,
-#                         return_tensors="pt",
-#                         padding=True,
-#                         truncation=True,
-#                         max_length=512,
-#                     )
-#                     outputs = model(**inputs)
-#                     exclusion_embedding = (
-#                         outputs.last_hidden_state[:, 0, :].detach().numpy()
-#                     )
+                    # append nctId and embedding data
+                    embedding_data.append(
+                        {
+                            "nctId": nctId,
+                            "treatment_embedding": treatment_embedding,
+                        }
+                    )
+                else:
+                    print(f"No valid description found for {study_id}")
 
-#                     # append nctId and embedding data
-#                     embedding_data.append(
-#                         {
-#                             "nctId": nctId,
-#                             "inclusion_embedding": inclusion_embedding,
-#                             "exclusion_embedding": exclusion_embedding,
-#                         }
-#                     )
-#                 else:
-#                     print(f"No valid description found for {study_id}")
+        except Exception as e:
+            print(f"An unexpected error occurred while processing {study_id}: {e}")
 
-#         except Exception as e:
-#             print(f"An unexpected error occurred while processing {study_id}: {e}")
-
-#     return embedding_data
+    return embedding_data
 
 
 def main():

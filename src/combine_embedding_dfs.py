bc2c33981b772bf9a2e86c7d2311fa75b6c99b99
97dankos <dankos@umich.edu>
2024-07-19 16:21:10 -0400

    fixed embeddings_df creation

--- src/combine_embedding_dfs.py
+++ src/combine_embedding_dfs.py
@@ -11,15 +11,31 @@ import re
 
 
 def combine_pickle_files(directory_path):
-    combined_df = pd.DataFrame()
+    df_list = []
 
     for file_name in os.listdir(directory_path):
         if file_name.endswith(".pkl"):
+            if file_name == "embeddings_df.pkl":
+                continue
+
             file_path = os.path.join(directory_path, file_name)
             with open(file_path, "rb") as f:
-                content = pickle.load(f)
-                if isinstance(content, pd.DataFrame):
-                    combined_df = pd.concat([combined_df, content], ignore_index=True)
+                p = pd.DataFrame(pickle.load(f))
+                df_list.append(p)
+
+    combined_df = pd.concat(df_list, axis=1, join="inner")
+    combined_df = combined_df.loc[:, ~combined_df.columns.duplicated()]
+
+    return combined_df
+
+
+def add_duration(directory_path):
+    duration = os.path.join(directory_path, "trial_duration.csv")
+    duration_df = pd.read_csv(duration)
+
+    combined_df = combine_pickle_files(directory_path)
+    combined_df = pd.merge(combined_df, duration_df, how="inner", on="nctId")
+
     return combined_df
 
 
@@ -27,7 +43,7 @@ def main():
     directory_path = "./data_example"
     output_path = "./data_example/embeddings_df.pkl"
 
-    embedding_data = combine_pickle_files(directory_path)
+    embedding_data = add_duration(directory_path)
     with open(output_path, "wb") as file:
         pickle.dump(embedding_data, file)
 

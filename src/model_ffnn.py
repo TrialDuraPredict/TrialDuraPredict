c06e62a885cdab8622ee219478f27b9951907e5c
Ubuntu <ubuntu@ip-172-31-22-113.ec2.internal>
2024-08-13 12:17:05 +0000

    add time, change batch_size

--- src/model_ffnn.py
+++ src/model_ffnn.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
 import pickle
-from tensorflow.keras.models import Sequential, load_model
+import time
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.callbacks import ModelCheckpoint
+
 
 # import dataset
 with open('./results/X_train.pkl', 'rb') as file:
@@ -28,12 +30,16 @@ model_ffnn = Sequential([
 # Compile the model
 model_ffnn.compile(optimizer='adam', loss='mean_squared_error')
 
-# Define callbacks (optional but recommended for saving the best model)
+# Define callbacks to save the best model
 checkpoint = ModelCheckpoint('./results/model_ffnn.keras', monitor='val_loss',
                              save_best_only=True, mode='min', verbose=1)
 
 # Train the model
+start_time = time.time()
 model_ffnn.fit(X_train, y_train,
-               epochs=50, batch_size=32,
+               epochs=50, batch_size=256, verbose=2,
                validation_data=(X_test, y_test),
                callbacks=[checkpoint])
+end_time = time.time()
+
+print("Total Time: {:.2f} mins".format((end_time - start_time)/60))

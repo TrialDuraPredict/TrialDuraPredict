8c898de9d35e0a6bbbd432a388b78f859e9ac6ab
Ubuntu <ubuntu@ip-172-31-22-113.ec2.internal>
2024-08-13 12:18:52 +0000

    add time, use pca reduced dataset, use n_jobs, etc

--- src/model_xgb.py
+++ src/model_xgb.py
@@ -3,18 +3,20 @@ import pandas as pd
 import pickle
 import xgboost as xgb
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
-from sklearn.externals import joblib
+import joblib
+import time
 
 # import dataset
-with open('./results/X_train.pkl', 'rb') as file:
-    X_train = pickle.load(file)
+with open('./results/X_train_pca.pkl', 'rb') as file:
+    X_train_pca = pickle.load(file)
 
 with open('./results/y_train.pkl', 'rb') as file:
     y_train = pickle.load(file)
 
 
 # Initialize the XGBoost regressor
-model_xgb = xgb.XGBRegressor(objective='reg:squarederror', random_state=42)
+model_xgb = xgb.XGBRegressor(objective='reg:squarederror',
+                             random_state=42, n_jobs=-1)
 
 
 # Tune the parameter
@@ -27,14 +29,25 @@ param_grid = {
 random_search = RandomizedSearchCV(
     estimator=model_xgb,
     param_distributions=param_grid, 
-    n_iter=10, cv=5,
+    n_iter=5, cv=5,
     scoring='neg_mean_squared_error',
     random_state=42
 )
 
-random_search.fit(X_train, y_train)
+start_time = time.time()
+random_search.fit(X_train_pca, y_train)
+end_time = time.time()
+
+# Print results
+print("Total Time for Randomized Search CV: {:.2f} mins".format((end_time - start_time)/60))
+print("Best Parameters:", random_search.best_params_)
+print("Best Score:", random_search.best_score_)
+
+# show details for each iteration
+for idx, (mean_test_score, params) in enumerate(zip(random_search.cv_results_['mean_test_score'],
+                                                    random_search.cv_results_['params'])):
+    print(f"Iteration {idx+1}: Score = {mean_test_score}, Params = {params}")
+    
+# save the best model
 best_model_xgb = random_search.best_estimator_
-
-
-# save model
-joblib.dump(best_model_rf, './results/model_xgb.sav')
+joblib.dump(best_model_xgb, './results/model_xgb.sav')

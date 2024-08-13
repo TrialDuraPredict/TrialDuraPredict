dd2aea51dc2b8fe839ef5f5bea6b6651d8d575d6
Ubuntu <ubuntu@ip-172-31-22-113.ec2.internal>
2024-08-13 12:18:18 +0000

    add time, use pca reudced dataset instead, add n_jobs, etc

--- src/model_rf.py
+++ src/model_rf.py
@@ -3,36 +3,48 @@ import pandas as pd
 import pickle
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
-from sklearn.externals import joblib
+import joblib
+import time
 
 # import dataset
-with open('./results/X_train.pkl', 'rb') as file:
-    X_train = pickle.load(file)
+with open('./results/X_train_pca.pkl', 'rb') as file:
+    X_train_pca = pickle.load(file)
 
 with open('./results/y_train.pkl', 'rb') as file:
     y_train = pickle.load(file)
 
-
 # Initialize the model
-model_rf = RandomForestRegressor(random_state=42)
+model_rf = RandomForestRegressor(random_state=42, n_jobs=-1)
 
 # Tune the parameter
 param_grid = {
-    'n_estimators': [100, 200, 300],
-    'max_depth': [None, 10, 20, 30],
+    'n_estimators': [50, 100, 200],
+    'max_depth': [10, 20, 30],
     'min_samples_leaf': [1, 5, 10]
 }
 
 random_search = RandomizedSearchCV(
     estimator=model_rf,
     param_distributions=param_grid, 
-    n_iter=10, cv=5,
+    n_iter=5, cv=5,
     scoring='neg_mean_squared_error',
     random_state=42
 )
 
-random_search.fit(X_train, y_train)
-best_model_rf = random_search.best_estimator_
+start_time = time.time()
+random_search.fit(X_train_pca, y_train)
+end_time = time.time()
 
-# save model
+# Print results
+print("Total Time for Randomized Search CV: {:.2f} mins".format((end_time - start_time)/60))
+print("Best Parameters:", random_search.best_params_)
+print("Best Score:", random_search.best_score_)
+
+# show details for each iteration
+for idx, (mean_test_score, params) in enumerate(zip(random_search.cv_results_['mean_test_score'],
+                                                    random_search.cv_results_['params'])):
+    print(f"Iteration {idx+1}: Score = {mean_test_score}, Params = {params}")
+    
+# save the best model
+best_model_rf = random_search.best_estimator_
 joblib.dump(best_model_rf, './results/model_rf.sav')
